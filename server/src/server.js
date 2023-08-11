cf4c3f21cf1dd4ce7787152d795fbc643f62e489
Md Habibur Rahman Shanto <mhrshanto36@gmail.com>
2023-08-12 01:16:19 +0600

    8/12/2023 last update

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const morgan = require('morgan')
 const app = express();
+
+
+app.use(morgan("dev"))
+
 
 app.get("/", (req, res) => {
     res.status(200).send("api is working fine");
 })
 
 app.listen(3010, () => {
-    console.log("Server is running successful")
+    console.log("Server is running working")
 });
